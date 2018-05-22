bf188758c7dc1889886c2dd7fead843fc1da9f7d
BitMineiro <bitmineiro@gmail.com>
2018-05-22 16:28:15 -0300

    Fix issue creating swap file.

--- CRC_MN_Install.sh
+++ CRC_MN_Install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#
-# Configuring environment
-#
+#=========================#
+# Configuring environment #
+#=========================#
 TMP_FOLDER=$(mktemp -d)
 CONFIG_FILE='crowdcoin.conf'
 #CONFIGFOLDER='/home/crowdcoin/.crowdcoincore' 
@@ -23,10 +23,30 @@ MYIP=$(curl -s4 icanhazip.com)
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
-#
-# End configuration
-#
+#===================#
+# End configuration #
+#===================#
 
+# Step 1)
+# Initial check if system is compatible and can be used as MN
+function checks() {
+if [[ $(lsb_release -d) != *16.04* ]]; then
+  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
+  exit 1
+fi
+
+if [[ $EUID -ne 0 ]]; then
+   echo -e "${RED}$0 must be run as root.${NC}"
+   exit 1
+fi
+
+if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "$COIN_DAEMOM" ] ; then
+  echo -e "${RED}$COIN_NAME is already installed.${NC}"
+  exit 1
+fi
+}
+
+# Step 2)
 # Create the user to install Master Node code
 function createUser() {
   echo "Creating $COIN_USER"
@@ -44,6 +64,66 @@ function createUser() {
   echo "Master Node will be installed on $CONFIGFOLDER"
   read a
 }
+
+# Step 3)
+# Download required packages and prepare the system
+function prepare_system() {
+echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
+apt-get update >/dev/null 2>&1
+DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
+DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
+apt install -y software-properties-common >/dev/null 2>&1
+echo -e "${GREEN}Adding bitcoin PPA repository"
+apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
+echo -e "Installing required packages, it may take some time to finish.${NC}"
+apt-get update >/dev/null 2>&1
+apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
+build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
+libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake ufw git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
+libminiupnpc-dev libgmp3-dev pkg-config libevent-dev libboost-all-dev libdb5.3++ libzmq5 unzip fail2ban >/dev/null 2>&1
+if [ "$?" -gt "0" ];
+  then
+    echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
+    echo "apt-get update"
+    echo "apt -y install software-properties-common"
+    echo "apt-add-repository -y ppa:bitcoin/bitcoin"
+    echo "apt-get update"
+    echo 'apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
+build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
+libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake ufw git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
+libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev libboost-all-dev libdb5.3++ libzmq5 unzip fail2ban'
+ exit 1
+fi
+clear
+}
+
+# Step 4)
+# Create swap file for virtual memory
+function create_swap() {
+ echo -e "Checking if swap space is needed."
+ PHYMEM=$(free -g|awk '/^Mem:/{print $2}')
+ SWAP=$(free -g|awk '/^Swap:/{print $2}')
+ if [ "$PHYMEM" -lt "2" ] && [ -n "$SWAP" ]
+ then
+    echo -e "${RED}Server is running with less than 2G of RAM without SWAP, do you want to create a 2G swap file?${NC}."
+    read -p "(Y)es / (N)o " -n 1 -r ANSWER
+    echo ""
+    if [[ $ANSWER =~ ^[Yy]$ ]]
+    then
+      fallocate -l 2G /swapfile
+      chmod 600 /swapfile
+      mkswap /swapfile
+      swapon /swapfile
+      echo -e "${GREEN}Configuring /etc/fstab...${NC}"
+      echo " "  >> /etc/fstab 
+      echo "/swapfile none swap sw 0 0" >> /etc/fstab
+    fi
+ else
+  echo -e "${GREEN}No swap file created.${NC}"
+ fi
+ clear
+}
+
 
 # Compile Masternode from source code (can take longer...)
 function compile_node() {
@@ -232,80 +312,6 @@ if [ "$?" -gt "0" ];
 fi
 }
 
-# Initial check if system is compatible and can be used as MN
-function checks() {
-if [[ $(lsb_release -d) != *16.04* ]]; then
-  echo -e "${RED}You are not running Ubuntu 16.04. Installation is cancelled.${NC}"
-  exit 1
-fi
-
-if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}$0 must be run as root.${NC}"
-   exit 1
-fi
-
-if [ -n "$(pidof $COIN_DAEMON)" ] || [ -e "$COIN_DAEMOM" ] ; then
-  echo -e "${RED}$COIN_NAME is already installed.${NC}"
-  exit 1
-fi
-}
-
-#Prepare the system and download packages required
-function prepare_system() {
-echo -e "Prepare the system to install ${GREEN}$COIN_NAME${NC} master node."
-apt-get update >/dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
-apt install -y software-properties-common >/dev/null 2>&1
-echo -e "${GREEN}Adding bitcoin PPA repository"
-apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
-echo -e "Installing required packages, it may take some time to finish.${NC}"
-apt-get update >/dev/null 2>&1
-apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
-build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
-libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake ufw git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
-libminiupnpc-dev libgmp3-dev pkg-config libevent-dev libboost-all-dev libdb5.3++ libzmq5 unzip fail2ban >/dev/null 2>&1
-if [ "$?" -gt "0" ];
-  then
-    echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
-    echo "apt-get update"
-    echo "apt -y install software-properties-common"
-    echo "apt-add-repository -y ppa:bitcoin/bitcoin"
-    echo "apt-get update"
-    echo 'apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" make software-properties-common \
-build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev libboost-program-options-dev \
-libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake ufw git wget curl libdb4.8-dev bsdmainutils libdb4.8++-dev \
-libminiupnpc-dev libgmp3-dev ufw pkg-config libevent-dev libboost-all-dev libdb5.3++ libzmq5 unzip fail2ban'
- exit 1
-fi
-clear
-}
-
-#Create swap file for virtual memory
-function create_swap() {
- echo -e "Checking if swap space is needed."
- PHYMEM=$(free -g|awk '/^Mem:/{print $2}')
- SWAP=$(free -g|awk '/^Swap:/{print $2}')
- if [ "$PHYMEM" -lt "2" ] && [ -n "$SWAP" ]
- then
-    echo -e "${RED}Server is running with less than 2G of RAM without SWAP, do you want to create a 2G swap file?${NC} [Y/N]"
-    read -e ANSWER
-    if [ $ANSWER =~ ^[Yy]$ ]
-    then
-      fallocate -l 2G /swapfile
-      chmod 600 /swapfile
-      mkswap /swapfile
-      swapon /swapfile
-      echo -e "${GREEN}Configuring /etc/fstab...${NC}"
-      echo " "  >> /etc/fstab 
-      echo "/swapfile none swap sw 0 0" >> /etc/fstab
-    fi
- else
-  echo -e "${GREEN}No swap file created.${NC}"
- fi
- clear
-}
-
 # Final 
 function important_information() {
  echo
@@ -415,5 +421,4 @@ read a
 echo "setup_node"
 read a
 setup_node
-
 
